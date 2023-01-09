4f8d046b3ce8f25bfdf936a777d6530708acfa5e
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-01-09 16:03:45 +0530

    Delete constants.js

--- src/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const baseUrl = 'https://api.themoviedb.org/3'
-export const API_KEY = '3f1ce0a79447fb2d091ae64b2c7b8245'
-export const imageUrl = 'https://image.tmdb.org/t/p/original'
-export const imageUrlLow = 'https://image.tmdb.org/t/p/w500'
